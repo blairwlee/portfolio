49c4586518aebde93d31eba8ad74e781bf360df9
blairwlee <98079092+blairwlee@users.noreply.github.com>
2022-01-30 22:33:21 +0900

    JS: Scroll into section

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  console.log(`navbarHeight: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   }
@@ -13,4 +12,26 @@ document.addEventListener('scroll', () => {
   }
 })
 
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  scrollIntoView(link);
+})
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+})
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ bahavior: 'smooth'});
+}
 
